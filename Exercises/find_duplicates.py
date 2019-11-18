db96c446bd45a1088285897aa06b2d770bc82222
Petru Conduraru <petruconduraru@outlook.com>
2019-11-18 19:58:00 +0100

    Updated solution

--- Exercises/find_duplicates.py
+++ Exercises/find_duplicates.py
@@ -3,7 +3,8 @@ some_list = ["a", "b", "c", "b", "d", "m", "n", "n"]
 
 duplicates = []
 for item in some_list:
-    if some_list.count(item) == 2 and duplicates.count(item) == 0:
-        duplicates.append(item)
+    if some_list.count(item) > 1:
+        if item not in duplicates:
+            duplicates.append(item)
 
 print(duplicates)
